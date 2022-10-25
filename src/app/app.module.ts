c56a51fd7171b7146cfaef26e378424ade5aff54
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-25 16:07:15 +0530

    t

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 const link:Routes=[
   {
@@ -19,7 +21,9 @@ const link:Routes=[
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(link)
+    RouterModule.forRoot(link),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-signin',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor(private myrouter:Router,private myapi:ApiService) { }
+  constructor(private myrouter:Router,private myapi:ApiService ) { }
 
   email=""
   pass=""
@@ -23,7 +25,7 @@ export class SigninComponent implements OnInit {
         if (response.length>0) {
           localStorage.setItem("name",response[0].name)
           localStorage.setItem("userid",response[0].id)
-          this.myrouter.navigate(["/tweet"])
+          this.myrouter.navigate(["/todo"])
         } else {
           alert("Invalid Credentials")
         }
